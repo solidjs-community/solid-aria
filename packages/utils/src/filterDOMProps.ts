77655556c26b823fa35816ed373bb28a600970c9
Fabien MARIE-LOUISE <fabien.marie-louise@symaris.fr>
2022-05-19 08:32:04 +0200

    chore: update copyright headers

--- packages/utils/src/filterDOMProps.ts
+++ packages/utils/src/filterDOMProps.ts
@@ -1,5 +1,10 @@
 /*
+ * Copyright 2022 Solid Aria Working Group.
+ * MIT License
+ *
+ * Portions of this file are based on code from react-spectrum.
  * Copyright 2020 Adobe. All rights reserved.
+ *
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy
  * of the License at http://www.apache.org/licenses/LICENSE-2.0
