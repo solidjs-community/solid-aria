e2c1d4255e76c6e2938cb7aa4c13567d9f19f462
Fabien MARIE-LOUISE <fabien.marie-louise@symaris.fr>
2022-05-22 16:25:30 +0200

    fix: export for SSR

---
+++ .changeset/fast-mirrors-wink.md
@@ -0,0 +1,7 @@
+---
+"@solid-aria/focus": patch
+"@solid-aria/overlays": patch
+"@solid-aria/primitives": patch
+---
+
+Fix export for SSR
--- packages/focus/rollup.config.js
+++ packages/focus/rollup.config.js
@@ -1,6 +1,6 @@
 import withSolid from "rollup-preset-solid";
 
 export default withSolid({
-  input: "src/index.ts",
+  input: "src/index.tsx",
   targets: ["esm", "cjs"]
 });
