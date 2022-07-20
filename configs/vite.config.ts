0afedbdf589c1d1f43d644d2ff02e3a562d63c0f
Damian Tarnawski <gthetarnav@gmail.com>
2022-07-20 17:09:12 +0000

    chore: add path aliases to `list` and `collection` packages

--- configs/vite.config.ts
+++ configs/vite.config.ts
@@ -1,8 +1,18 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
+const pathToPackages = resolve(__dirname, "..", "packages");
+const resolvePackage = (name: string) => resolve(pathToPackages, name, "src");
+
 export const viteConfig = defineConfig({
   plugins: [solidPlugin()],
+  resolve: {
+    alias: {
+      "@solid-aria/list": resolvePackage("list"),
+      "@solid-aria/collection": resolvePackage("collection")
+    }
+  },
   build: {
     target: "esnext",
     polyfillDynamicImport: false
